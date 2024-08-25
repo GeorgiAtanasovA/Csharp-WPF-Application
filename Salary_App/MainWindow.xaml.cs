64fc3c48ee9ac98207222c31897366f3bb8b5216
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-08-25 23:04:45 +0300

    Add files via upload

---
+++ Salary_App/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ZaplataCompute
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class SalaryCompute : Window
+   {
+      public SalaryCompute()
+      {
+         InitializeComponent();
+         dateTime.Content = DateTime.Now;
+         labelInfo.Content = labelInfo.Content + "\nHi, write your hourly wage, your hours and your tax.";
+      }
+
+      private void button_Click(object sender, RoutedEventArgs e)
+      {
+         try
+         {
+            double hourlyWage = double.Parse(hourlyWageTB.Text);
+            double hours = double.Parse(timeTB.Text);
+            double skatt = double.Parse(taxTB.Text);
+
+            double sum = hourlyWage / 100;
+            sum = sum * skatt;
+            sum = hourlyWage - sum;
+            sum = sum * hours;
+
+            labelInfo.Content = "";
+            labelInfo.Content = "Info:";
+            labelInfo.Content = labelInfo.Content + "\nHi, write your hourly wage, your hours and your tax.";
+
+            resultL.HorizontalContentAlignment = HorizontalAlignment.Left;
+            resultL.Content = "      Amount to receive:";
+            resultLDigit.Content = sum;
+         }
+         catch (Exception)
+         {
+            labelInfo.Content = "Enter numbers in all fields!";
+         }
+      }
+
+      private void calcB_Click(object sender, RoutedEventArgs e)
+      {
+         Process.Start("Calc");
+      }
+
+      private void button_Click_1(object sender, RoutedEventArgs e)
+      {
+         hourlyWageTB.Text = "";
+         timeTB.Text = "";
+         taxTB.Text = "";
+         resultL.HorizontalContentAlignment = HorizontalAlignment.Center;
+         resultL.Content = "Result:";
+         resultLDigit.Content = "";
+      }
+   }
+}
