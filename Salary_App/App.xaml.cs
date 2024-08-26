2d5f7bcde4981054b46165dd8466bd37d7e93b56
Georgi <124593129+GeorgiAtanasovA@users.noreply.github.com>
2024-08-26 11:17:18 +0300

    Add files via upload

---
+++ Salary_App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace _Salary_App
+{
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application
+   {
+   }
+}
